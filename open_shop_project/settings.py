e8739d9f4863542075bd4e4fcccff2eeca4c3ad2
hafidz <hnpradigda@gmail.com>
2026-03-30 02:22:21 +0700

    Update Pipfile and Pipfile.lock for package versions; modify ALLOWED_HOSTS in settings.py

--- open_shop_project/settings.py
+++ open_shop_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-su1q8z9omds6#u-*^=^t!(wpe$-a!u3)3x0x)=3*5-z_(1mc0%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
